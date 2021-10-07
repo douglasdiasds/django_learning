49a22e574d4459cc995b8e6f99891a50b4ae9b2f
douglasdiasds <diasdasilvadouglas12@gmail.com>
2021-10-07 16:00:17 -0300

    CMMT_09: Adding Content serializer & viewset

--- content/views/views.py
+++ content/views/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.views import APIView
 
-# Create your views here.
+from content.models import Content
+from content.serializer.serializer import ContentSerializer
+
+class ContentViewSet(viewsets.ModelViewSet):
+
+    def get_queryset(self):
+        return Content.objects.all()
+
+    def get_serializer_class(self):
+        return ContentSerializer
+
+
